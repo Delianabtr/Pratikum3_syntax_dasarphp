70bc3b778c9c297108bd40846134e7b0c1b62278
deliana <delianabtr30@gmail.com>
2021-09-21 10:30:28 +0700

    membuat perpindahan dengan menggunakan break

---
+++ do-while.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perulangan dengan memakai do_while</title>
+</head>
+<body>
+    <?php
+        $i =2;
+        do
+        {
+            echo "\$i = $i <BR>";
+            $i++;
+        }
+        while ($i < 5);
+    ?>
+    
+</body>
+</html>
